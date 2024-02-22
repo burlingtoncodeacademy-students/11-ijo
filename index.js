9b104e540733fe5fb7a9e06cfc5d6cb23196ce5a
Chris Boydstun <52934303+cboydstun@users.noreply.github.com>
2024-02-22 17:56:56 -0600

    two inputs

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-console.log("hello world")
+process.stdin.once("data", (input) => {
+    console.log(input.toString());
+});
+
+function printLine(chris){
+    console.log(chris.toString());
+}
+
+process.stdin.once("data", printLine);
