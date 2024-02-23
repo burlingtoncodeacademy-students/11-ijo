e9d310efea6608a2e351a3484ec1b9e915678a70
Chris Boydstun <52934303+cboydstun@users.noreply.github.com>
2024-02-22 18:20:47 -0600

    stdin and callbacks

--- index.js
+++ index.js
@@ -3,7 +3,27 @@ process.stdin.once("data", (input) => {
 });
 
 function printLine(chris){
-    console.log(chris.toString());
+    console.log(chris);
 }
 
 process.stdin.once("data", printLine);
+
+function moreCowBell(callback){
+    callback();
+}
+
+function moreCallback(callback){
+    moreCowBell(callback);
+}
+
+function anotherCallback(callback){
+    moreCallback(callback);
+}
+
+function useCallback(callback){
+    anotherCallback(callback);
+}
+
+useCallback(function(){
+    console.log("Three callbacks and a cow bell deep!!");
+})
